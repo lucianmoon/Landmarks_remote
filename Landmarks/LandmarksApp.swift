edc6723209f51adf2029b3b0b31f15e5005fa24b
lowjungxuan <abu@umchtech.com>
2022-02-12 17:40:57 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Low Jung Xuan on 12/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
